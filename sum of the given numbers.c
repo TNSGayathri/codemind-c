be226e28cd26bda298718b6f3512189925134187
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-01-22 07:12:26 +0530

    #include<stdio.h> #include<math.h> int main() {     int i=1,n,a,b,s;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d%d",&a,&b);         s=a+b;         printf("%d ",s);     }
    }

---
+++ sum of the given numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i=1,n,a,b,s;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d%d",&a,&b);
+        s=a+b;
+        printf("%d
+",s);
+    }
+    
+}

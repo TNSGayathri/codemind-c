de6334c6a1ab3541db148c3104c5885c4e03f3ce
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-01-23 11:28:28 +0530

    #include<stdio.h> int main() {     int n,r,large=0;     scanf("%d",&n);     while(n>=10)     {         r=n%10;         n=n/10;         if(large<r)         {             large=r;         }         else         {             large=large;         }     }     if(n<10)     {         r=n%10;         n=n/10;         if(large<r)         {             large=r;         }         else         {             large=large;         }     }     printf("%d",large); }

---
+++ Largest_Digit.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,large=0;
+    scanf("%d",&n);
+    while(n>=10)
+    {
+        r=n%10;
+        n=n/10;
+        if(large<r)
+        {
+            large=r;
+        }
+        else
+        {
+            large=large;
+        }
+    }
+    if(n<10)
+    {
+        r=n%10;
+        n=n/10;
+        if(large<r)
+        {
+            large=r;
+        }
+        else
+        {
+            large=large;
+        }
+    }
+    printf("%d",large);
+}

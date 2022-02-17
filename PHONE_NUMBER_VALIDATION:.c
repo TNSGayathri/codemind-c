5474cdd794b28de133a21e7007b0fe91eea1cc72
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-02-17 18:34:42 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[10];     scanf("%s",str);     if(str[1]!=0&&strlen(str)==10)     {         printf("Valid");     }     else     {         printf("Invalid");     }
    }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[10];
+    scanf("%s",str);
+    if(str[1]!=0&&strlen(str)==10)
+    {
+        printf("Valid");
+    }
+    else
+    {
+        printf("Invalid");
+    }
+    
+}
